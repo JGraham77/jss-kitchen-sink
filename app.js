d91d4f467ced77401fb0c6d5ef2015e942c281cf
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-01-13 13:12:38 -0600

    completing lab step 7

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var myName = 'Joshua Graham';
-
+// This var is my name
 const states = 50;
-
+// This const states the number of states in the USA
 var add = (5 + 4);
-
+// This var adds 5 and 4 together
