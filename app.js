321e6c1e4048ceed42be79049a97ff36ae2ec930
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-01-13 16:45:59 -0600

    completing lab step 8

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const states = 50;
 // This const states the number of states in the USA
 var add = (5 + 4);
 // This var adds 5 and 4 together
+
+function sayHello() {
+    console.log('Hello World');
+}
+sayHello();
+
